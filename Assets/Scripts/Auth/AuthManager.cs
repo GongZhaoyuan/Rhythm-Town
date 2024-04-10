50f67c12407cf2066ad1124a19c2c14dbbc0034d
Tina WU <90884331+Tina-WU2021@users.noreply.github.com>
2024-04-10 16:37:30 +0800

    SignUp to avatar choosing success

--- Assets/Scripts/Auth/AuthManager.cs
+++ Assets/Scripts/Auth/AuthManager.cs
@@ -8,24 +8,32 @@ using UnityEngine;
 
 public class AuthManager : MonoBehaviour
 {
+    public GameObject AvatarChoosing;
+    public GameObject SignUpPanel;
+
     const string LAST_USERNAME_KEY = "LAST_USERNAME",
         LAST_PASSWORD_KEY = "LAST_PASSWORD";
 
     [SerializeField]
-    TMP_Text signUpNickName;
+    TMP_Text signUpEmailAsUserName;
 
     [SerializeField]
     TMP_Text signUpPassword;
 
     [SerializeField]
-    TMP_Text logInNickName;
+    TMP_Text logInEmail;
 
     [SerializeField]
     TMP_Text logInPassword;
 
-    public async void OnRegisterPressed()
+    public async void On_SignUp_SignUpPressed()
     {
-        await SignUpWithUsernamePasswordAsync(signUpNickName.text, signUpPassword.text);
+        await SignUpWithUsernamePasswordAsync(signUpEmailAsUserName.text, signUpPassword.text);
+    }
+
+    public async void On_LogIn_LogInPressed()
+    {
+        await SignInWithUsernamePasswordAsync(logInEmail.text, logInPassword.text);
     }
 
     async Task SignUpWithUsernamePasswordAsync(string username, string password)
@@ -33,11 +41,41 @@ public class AuthManager : MonoBehaviour
         await UnityServices.InitializeAsync();
         try
         {
+            Debug.Log("username:" + username + " password: " + password);
             await AuthenticationService.Instance.SignUpWithUsernamePasswordAsync(
                 username,
                 password
             );
             Debug.Log("SignUp is successful.");
+            Debug.Log("PlayerID: " + AuthenticationService.Instance.PlayerId);
+            SignUpPanel.gameObject.SetActive(false);
+            AvatarChoosing.gameObject.SetActive(true);
+        }
+        catch (AuthenticationException ex)
+        {
+            // Compare error code to AuthenticationErrorCodes
+            // Notify the player with the proper error message
+            Debug.LogException(ex);
+        }
+        catch (RequestFailedException ex)
+        {
+            // Compare error code to CommonErrorCodes
+            // Notify the player with the proper error message
+            Debug.LogException(ex);
+        }
+    }
+
+    async Task SignInWithUsernamePasswordAsync(string username, string password)
+    {
+        await UnityServices.InitializeAsync();
+        try
+        {
+            Debug.Log("username:" + username + " password: " + password);
+            await AuthenticationService.Instance.SignInWithUsernamePasswordAsync(
+                username,
+                password
+            );
+            Debug.Log("SignIn is successful.");
             Debug.Log("PlayerID: " + AuthenticationService.Instance.PlayerId);
         }
         catch (AuthenticationException ex)
