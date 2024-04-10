94e3bfa09ea32491d97832d01e969f54b45f275a
kataru_ <108117091+kataru0524@users.noreply.github.com>
2024-04-10 19:03:44 +0800

    Game End Interface

---
+++ Assets/Scripts/Game/GameSettings.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSettings : MonoBehaviour
+{
+    public bool isInfinite;
+    public int difficulty;
+    public int skillNo;
+    public int skillLevel;
+}
