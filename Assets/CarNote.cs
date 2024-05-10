e013c0307ad43ad0336fa832bcd4cf4f86656e29
GongZhaoyuan <140373573+GongZhaoyuan@users.noreply.github.com>
2024-05-11 01:01:53 +0800

    economy System

---
+++ Assets/CarNote.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarNote : NoteControllerBackUp
+{
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+
+        noteType = Random.Range(0, 3);
+        Sprite[] sprites = Resources.LoadAll<Sprite>("car");
+        Debug.Log("sprites: " + sprites);
+        targetSource = $"Cardboard Box_{noteType}";
+        destination = new Vector2(-99, -99);
+        display.GetComponent<SpriteRenderer>().sprite = sprites[noteType];
+        if (!isTarget)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    protected override void MoveToDestination(string clickSource)
+    {
+        base.MoveToDestination(clickSource);
+        displayRb.gameObject.GetComponent<Animator>().SetTrigger("Moved");
+    }
+}
